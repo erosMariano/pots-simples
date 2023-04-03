734463145ab3c055f863097c31892d01525c9d7c
erosMariano <erosmariano1@gmail.com>
2023-04-03 17:14:50 -0300

    first commit

---
+++ web-share-api/script.js
@@ -0,0 +1,12 @@
+if (navigator.share) {
+  navigator.share({
+      title: "Aplicação Exemplo",
+      text: "Essa é uma aplicação exemplo para desenvolvedores ",
+      url: "https://github.com/",
+    })
+    .then(() => {
+      console.log("Compartilhado com Sucesso");
+    }).catch((error) =>{
+      console.log("Erro ao compartilhar",  error)
+    }) 
+}
