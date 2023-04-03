bc824827e1540b3b53975dec6c2fb523648583f7
erosMariano <erosmariano1@gmail.com>
2023-04-03 17:17:15 -0300

    feat: button

--- web-share-api/script.js
+++ web-share-api/script.js
@@ -1,12 +1,15 @@
-if (navigator.share) {
-  navigator.share({
-      title: "Aplicação Exemplo",
-      text: "Essa é uma aplicação exemplo para desenvolvedores ",
-      url: "https://github.com/",
-    })
-    .then(() => {
-      console.log("Compartilhado com Sucesso");
-    }).catch((error) =>{
-      console.log("Erro ao compartilhar",  error)
-    }) 
-}
+document.querySelector(".button").addEventListener("click", () => {
+  if (navigator.share) {
+    navigator.share({
+        title: "Aplicação Exemplo",
+        text: "Essa é uma aplicação exemplo para desenvolvedores ",
+        url: "https://github.com/",
+      })
+      .then(() => {
+        console.log("Compartilhado com Sucesso");
+      })
+      .catch((error) => {
+        console.log("Erro ao compartilhar", error);
+      });
+  }
+});
